8444008fa7bc2aa70cf87aa13e4520ecc07a1753
Braden <braden.douglass@gmail.com>
2013-02-15 13:26:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MCGHomeWork::Application.config.secret_token = '4f888b1e495f8c6657bfaf7d923a22fdbf54a6893059257863ca5e66dbabbb6707952ad300c55c3f02721536f90583d072e5192f9434c7131396db8b1f61f3e2'
